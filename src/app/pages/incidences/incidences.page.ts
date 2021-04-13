0dcfb23a8fbb42c10be44c02b3d5d0df00357c6a
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-04-13 14:44:01 -0400

    uso de iconocgrafia

--- src/app/pages/incidences/incidences.page.ts
+++ src/app/pages/incidences/incidences.page.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 } )
 export class IncidencesPage implements OnInit {
 
+  icon = './../../../assets/svg/chat.svg';
   constructor() { }
 
   ngOnInit() {
